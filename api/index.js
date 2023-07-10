11fc88d98d2c5949737f617a7790520e7c8b3b16
Roteesaimai <107842127+Roteesaimai1@users.noreply.github.com>
2023-07-10 18:01:37 +0700

    frist crud

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ module.exports = router
 //set api กำหนดเส้นทาง
 router.use('/', require('./home'))
 router.use('/home', require('./home'))
-router.use('/addtask', require('./addtask'))
-router.use('/deletetask', require('./deletetask'))
-router.use('/updatetask', require('./updatetask'))
+router.use('/menu', require('./menu'))
+router.use('/addtask', require('./addtask')) /* demo */
+router.use('/deletetask', require('./deletetask')) /* demo */
+router.use('/updatetask', require('./updatetask')) /* demo */
 /* router.use('/fishdata', require('./fishdata')) */
