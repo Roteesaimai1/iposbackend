c447483bc246843ee89b8205eb2ae719a1194ad9
Roteesaimai <107842127+Roteesaimai1@users.noreply.github.com>
2023-07-14 02:56:08 +0700

    editRoutes

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 global.Menu = require('./api/models/menuModel');
+global.User = require('./api/models/userModel');
 const routes = require('./api/routes/menuRoutes');
+const UserRoutes = require('./api/routes/userRoutes');
 
 mongoose.connect(
   'mongodb://127.0.0.1:27017/iposdb',
@@ -17,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app);
+UserRoutes(app);
 app.listen(port);
 
 app.use((req, res) => {
